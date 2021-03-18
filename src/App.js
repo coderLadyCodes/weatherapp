50174545bd4bbaf72f660c9d677d4ab3d2d374ec
jeSuisCodeuse <creativegirl@outlook.fr>
2021-03-18 15:39:37 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+const api= {
+  key: "682e64261ac4049f023c126b080b40cf",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      This is a weather App built with react js
-    </div>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="search..."
+          />
+        </div>
+         
+      </main>
+     </div>
   );
 }
 
