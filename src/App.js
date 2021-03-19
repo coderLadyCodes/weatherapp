d724270d503d7f605111c563153fd6320e8d66a5
jeSuisCodeuse <creativegirl@outlook.fr>
2021-03-19 22:42:35 +0100

    css done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api= {
   key: "682e64261ac4049f023c126b080b40cf",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather,setWeather] = useState({});
+
+  const dateBuilder = (d) => {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday',  'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+  
+  
   return (
     <div className="app">
       <main>
@@ -15,7 +33,18 @@ function App() {
           placeholder="search..."
           />
         </div>
-         
+         <div className="location-box">
+           <div className="location">Avignon, France</div>
+           <div className="date">{dateBuilder(new Date())}</div>
+         </div>
+         <div className="weather-box">
+           <div className="temp">
+            15°C
+           </div>
+           <div className="weather">
+             Sunny
+           </div>
+         </div>
       </main>
      </div>
   );
