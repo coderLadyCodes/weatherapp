b8b6e02788240b5e1dfcb7d692f893c654cbf32b
jeSuisCodeuse <creativegirl@outlook.fr>
2021-04-02 16:01:33 +0200

    end

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather,setWeather] = useState({});
 
+  const search = evt =>{
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metrica&APPID=${api.key}`)
+      .then(resp => resp.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+      });
+        
+    }
+  }
+
   const dateBuilder = (d) => {
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
     'August', 'September', 'October', 'November', 'December'];
@@ -24,27 +36,32 @@ function App() {
   
   
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app hot' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
           type="text"
           className="search-bar"
           placeholder="search..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
           />
         </div>
-         <div className="location-box">
-           <div className="location">Avignon, France</div>
+        {(typeof weather.main != "undefined") ? (
+         <div>
+           <div className="location-box">
+           <div className="location">{weather.name}, {weather.sys.country}</div>
            <div className="date">{dateBuilder(new Date())}</div>
          </div>
          <div className="weather-box">
            <div className="temp">
-            15°C
+             {Math.round(weather.main.temp)}°c
            </div>
-           <div className="weather">
-             Sunny
-           </div>
+           <div className="weather">{weather.weather[0].main}</div>
          </div>
+         </div>
+         ) : ('')}
       </main>
      </div>
   );
